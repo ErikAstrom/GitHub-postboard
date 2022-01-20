c4fcb85a02ca72a10182cfc24ca10ebbe90cdb07
Erik Åström <erik@stateless.digital>
2022-01-20 20:30:02 +0100

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  return (
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default App;
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+
+
+import prisma from '../../../lib/prisma';
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+    adapter: PrismaAdapter(prisma),
+
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        })
+      ],
+}
